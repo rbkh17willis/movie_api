8aa347c190d1cd725b7ca2adb4b16c62bf63a0ac
rbkh17willis <124805813+rbkh17willis@users.noreply.github.com>
2023-12-01 10:42:25 -0700

    saving token for posted users

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
 
-mongoose.connect( process.env.CONNECTION_URI,
+mongoose.connect(process.env.CONNECTION_URI,
   { 
     useNewUrlParser: true, 
     useUnifiedTopology: true 
@@ -181,6 +181,45 @@ app.post('/users',
         res.status(500).send('Error: ' + error);
       });
   });
+
+  //saving token for posted users
+  const jwtSecret = 'your_jwt_secret'; // This has to be the same key used in the JWTStrategy
+
+const jwt = require('jsonwebtoken'),
+  passport = require('passport');
+
+require('./passport'); // Your local passport file
+
+
+let generateJWTToken = (user) => {
+  return jwt.sign(user, jwtSecret, {
+    subject: user.Username, // This is the username you’re encoding in the JWT
+    expiresIn: '7d', // This specifies that the token will expire in 7 days
+    algorithm: 'HS256' // This is the algorithm used to “sign” or encode the values of the JWT
+  });
+}
+
+
+/* POST login. */
+module.exports = (router) => {
+  router.post('/login', (req, res) => {
+    passport.authenticate('local', { session: false }, (error, user, info) => {
+      if (error || !user) {
+        return res.status(400).json({
+          message: 'Something is not right',
+          user: user
+        });
+      }
+      req.login(user, { session: false }, (error) => {
+        if (error) {
+          res.send(error);
+        }
+        let token = generateJWTToken(user.toJSON());
+        return res.json({ user, token });
+      });
+    })(req, res);
+  });
+}
 
 // PUT/UPDATE user info
 app.put('/users/:Username', passport.authenticate('jwt', { session: false }), async (req, res) => {
